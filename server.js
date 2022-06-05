1a90c934429d6ba61da1cf1f719d8c9796a114fd
YilaiYan <yanyilai201708@gmail.com>
2022-06-04 20:41:23 -0500

    simple server

---
+++ server.js
@@ -0,0 +1,10 @@
+//bring in express
+const express = require('express');
+//initialize aoo variable with express
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
